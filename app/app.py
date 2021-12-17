7cfb3252b8b4d2c847a41a768a6b72c9516c7e41
Valerian <v.levinski@gmail.com>
2021-12-17 11:29:24 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,12 +3,14 @@ from mysql.connector import connect, Error
 
 # settings
 host = "0.0.0.0"
+port = 3306
 
 try:
-    with connect(host,
+    with connect(host=host,
+                 port=port,
                  user=input("Enter username: "),
                  password=getpass("Enter password: "),
                  ) as connection:
-        print(connection)
+        print("Connector --> ", connection)
 except Error as e:
     print(e)
